9fef8e8820ca5d63a6b507e4618a404f875b4415
Gabe <gabriel-soares@uergs.edu.br>
2024-09-04 23:49:42 +0000

    add: decl macros with repetitions

--- advanced/src/m1_decl_macros.rs
+++ advanced/src/m1_decl_macros.rs
@@ -27,4 +27,25 @@ mod tests {
         let some_var: String = ex_type_macro!(u8);
         dbg!(some_var);
     }
+
+    macro_rules! my_vec {
+        ( $($x: expr),+) => {
+          {
+            let mut temp_vec = Vec::new();
+            $(
+              temp_vec.push($x);
+            )+
+            temp_vec
+          }
+        };
+    }
+
+    #[test]
+    fn tests_decl_macros_vec() {
+        let x: Vec<i32> = vec![1, 2, 3];
+        dbg!(x);
+
+        let y: Vec<i32> = my_vec!(3, 2, 1);
+        dbg!(y);
+    }
 }
