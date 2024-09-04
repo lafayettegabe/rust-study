3c1691ce0ba6440d94e69547fed52e27fc359d39
lafayettegabe <gabriel-soares@uergs.edu.br>
2024-09-04 01:55:44 -0300

    add: pattern matching - enum, guards and structs

--- intermediate/src/m6_patterns.rs
+++ intermediate/src/m6_patterns.rs
@@ -1,3 +1,28 @@
+#[derive(Debug)]
+enum Message {
+    Quit,
+    ChangeColour(i32, i32, i32),
+    Move { x: i32, y: i32 },
+    Write(String),
+}
+
+fn procress_message(msg: Message) {
+    match msg {
+        Message::Quit => {
+            println!("I quit!");
+        }
+        Message::ChangeColour(red, green, blue) => {
+            println!("Red {}, Green {}, Blue {}", red, green, blue);
+        }
+        Message::Move { x, y } => {
+            println!("X is {}, Y is {}", x, y);
+        }
+        Message::Write(text) => {
+            println!("{}", text)
+        }
+    };
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -73,5 +98,47 @@ mod test {
         };
 
         println!("{}", my_int);
+    }
+
+    #[test]
+    fn tests_large_enum() {
+        let my_quit = Message::Quit;
+        procress_message(my_quit);
+
+        let my_colour = Message::ChangeColour(10, 20, 255);
+        procress_message(my_colour);
+
+        let my_move = Message::Move { x: 10, y: 200 };
+        procress_message(my_move);
+
+        let my_write = Message::Write("Hello World!".to_string());
+        procress_message(my_write);
+    }
+
+    #[test]
+    fn tests_match_guard() {
+        let pair = (2, -2);
+        match pair {
+            (x, y) if x == y => println!("They match!"),
+            (x, y) if x + y == 0 => println!("They neutralize"),
+            (_, y) if y == 2 => println!("Y is indeed +2"),
+            _ => println!("Dont care..."),
+        }
+    }
+
+    #[test]
+    fn tests_match_struct() {
+        struct Location {
+            x: i32,
+            y: i32,
+        }
+
+        let location = Location { x: 0, y: 20 };
+
+        match location {
+            Location { x, y: 0 } => println!("Y is on the axis"),
+            Location { x: 0, y } => println!("X is on the axis"),
+            Location { x, y } => println!("X and Y are not on the axis"),
+        }
     }
 }
