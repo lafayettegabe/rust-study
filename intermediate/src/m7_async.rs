4851f65281845015891713507977c5e1ad35e0d0
Gabe <gabriel-soares@uergs.edu.br>
2024-09-04 20:54:33 +0000

    add: async api

--- intermediate/src/m7_async.rs
+++ intermediate/src/m7_async.rs
@@ -1,9 +1,23 @@
+async fn my_async_call(url: &str) -> Result<serde_json::Value, reqwest::Error> {
+    let response: serde_json::Value = reqwest::get(url).await?.json::<serde_json::Value>().await?;
+    Ok(response)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[tokio::test]
     async fn tests_calls_async_fn() {
-        dbg!("Hello...");
+        let api_url = "https://cat-fact.herokuapp.com/facts/";
+        let my_res = my_async_call(api_url).await;
+        match my_res {
+            Ok(r) => {
+                dbg!(r);
+            }
+            Err(_) => {
+                panic!("Failed to make request!");
+            }
+        };
     }
 }
--- intermediate/src/main.rs
+++ intermediate/src/main.rs
@@ -1,9 +1,11 @@
+/*
 mod m1_enums;
 mod m2_structs;
 mod m3_traits;
 mod m4_polymorphism;
 mod m5_lifetimes;
 mod m6_patterns;
+*/
 mod m7_async;
 
 fn main() {
